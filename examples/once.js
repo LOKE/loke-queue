6f2eab369c06abe65247e7c9064274a23e36fb3d
Anthony Foster <git@af.id.au>
2015-11-18 17:44:07 +1100

    Add Connection#close method

---
+++ examples/once.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var Connection = require('../').Connection
+
+var connection = new Connection('amqp://localhost')
+
+var queue = connection.queue('blah', {maxConcurrent: 20})
+
+queue.subscribe('message', function (message) {
+  console.log(message)
+  return new Promise(function (resolve) {
+    setTimeout(resolve, 500)
+  })
+})
+
+queue.on('ready', function () {
+  connection.publish('message', {
+    message: 'Hello World'
+  })
+
+  queue.stop()
+  .then(function () {
+    return connection.close()
+  })
+  return
+})
